4e53cf9ffa427420386c0e439dd31e11f6c60c2f
adilet <addik.koishibek03@gmail.com>
2024-12-23 01:01:13 +0500

    delete and add in database

--- src/pages/TreatmentPage.jsx
+++ src/pages/TreatmentPage.jsx
@@ -1,47 +1,23 @@
 import {useEffect, useState} from "react";
 import {
     Box,
-    Card,
-    CardContent,
     Typography,
-    IconButton,
     Button, Grid2 as Grid,
 } from "@mui/material";
 import ActivityModal from "../components/ActivityModal.jsx";
-import {getMedicationUnits} from "../api/api.js";
+import {deleteMedicationReminder, getMedicationReminders, saveMedicationRemind} from "../api/api.js";
 import {useNavigate} from "react-router";
 import {LOGIN_URL} from "../api/url.js";
 import MedicationReminderCard from "../components/MedicationReminderCard.jsx";
 
 
-const reminders = [{
-    id: 1,
-    title: "Medication",
-    scheduleType: "MULTIPLE_TIMES_DAY",
-    unitId: 1, // 1 = mg
-    entries: [
-        {cron: "0 8 * * *", dose: 10},
-        {cron: "0 14 * * *", dose: 10},
-        {cron: "0 20 * * *", dose: 10},
-    ],
-},
-    {
-        id: 2,
-        title: "Medication",
-        scheduleType: "MULTIPLE_TIMES_DAY",
-        unitId: 1, // 1 = mg
-        scheduleData: [
-            {cron: "0 8 * * *", dose: 10},
-            {cron: "0 14 * * *", dose: 10},
-            {cron: "0 20 * * *", dose: 10},
-        ],
-    }
-];
-
-
 const TreatmentPage = () => {
+    const navigate = useNavigate();
+
     const [modalOpen, setModalOpen] = useState(false);
     const [selectedActivity, setSelectedActivity] = useState(null);
+
+    const [reminders, setReminders] = useState([]);
 
     const handleModalOpen = (type) => {
         setSelectedActivity({type}); // Initialize activity type
@@ -54,9 +30,45 @@ const TreatmentPage = () => {
     };
 
     const handleSaveActivity = (formData) => {
-        reminders.push({...formData, id: 3});
+        const token = localStorage.getItem("accessToken");
+        console.log(JSON.stringify(formData));
+        saveMedicationRemind(token, formData).then(response => {
+            if (response.status === 200) {
+                return response.json();
+            }
+            if (response.status === 401) {
+                navigate(LOGIN_URL);
+                return;
+            }
+            console.error(response.body)
+        }).then(data => {
+            setReminders([...reminders, {...formData, id: data.id}]);
+        }).catch(error => {
+            console.error(error.message);
+        })
         handleModalClose();
     };
+
+    useEffect(() => {
+        const token = localStorage.getItem("accessToken");
+        getMedicationReminders(token)
+            .then(response => {
+                if (response.status === 200) {
+                    return response.json();
+                }
+                if (response.status === 401) {
+                    navigate(LOGIN_URL);
+                    return;
+                }
+                console.error(response.body);
+            })
+            .then(data => {
+                setReminders([...data]);
+            })
+            .catch(error => {
+                console.error(error.message);
+            });
+    }, []);
 
     return (
         <Box p={3}>
@@ -75,12 +87,27 @@ const TreatmentPage = () => {
 
             <Grid container spacing={3} sx={{pb: 3}}>
                 {reminders.map((reminder) => (
-                    <Grid key={reminder.id} size={{xs: 12, sm: 12,  md: 6, lg: 4, xl: 3}}>
+                    <Grid key={reminder.id} size={{xs: 12, sm: 12, md: 6, lg: 4, xl: 3}}>
                         <MedicationReminderCard
                             medicationReminder={reminder}
                             onEdit={() => {
+
                             }}
                             onRemove={() => {
+                                const token = localStorage.getItem('accessToken');
+                                deleteMedicationReminder(token, reminder.id)
+                                    .then(response => {
+                                        if (response.status === 200) {
+                                            setReminders(reminders.filter(r => r.id !== reminder.id));
+                                        } else if (response.state === 401) {
+                                            navigate(LOGIN_URL);
+                                        } else {
+                                            console.error(response.body)
+                                        }
+                                    })
+                                    .catch(error => {
+                                        console.error(error.message);
+                                    });
                             }}
                         />
                     </Grid>
